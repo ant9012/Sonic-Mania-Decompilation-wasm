8f5c3fe039c058b58f199ed974231f696e50f59e
TheSmallAnto <antd39487@gmail.com>
2026-04-01 05:42:59 -0400

    update flags

--- RSDK-Library-GameAPI.cmake
+++ RSDK-Library-GameAPI.cmake
@@ -16,12 +16,16 @@ set(emsc_link_options
     -sLINKABLE=1
     -sEXPORT_ALL=1
     -sSIDE_MODULE=2
-    -sSINGLE_FILE=1
-    -sBINARYEN_ASYNC_COMPILATION=0
+    # Fix 1: Match Asyncify with the main module to prevent stack corruption
+    # when async call paths cross the module boundary
+    -sASYNCIFY
+    -sASYNCIFY_STACK_SIZE=65536
     -sUSE_PTHREADS=1
     -sPTHREAD_POOL_SIZE=4
     -pthread
     -g
+    # REMOVED: -sSINGLE_FILE=1        (meaningless on a side module)
+    # REMOVED: -sBINARYEN_ASYNC_COMPILATION=0  (conflicts with pthreads + Asyncify)
 )
 
 target_link_options(${GAME_NAME} PRIVATE ${emsc_link_options})
